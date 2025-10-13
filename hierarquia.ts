f668c6b681e6b40657a4aa142a1196374d8d91c7
Rafael Machado <rafaelmachadocd37@gmail.com>
2025-10-13 11:54:10 -0300

    commit da silva

--- hierarquia.ts
+++ hierarquia.ts
@@ -1,5 +1,12 @@
 interface Animal{
   tipoDePelagem: string
   nome: string
-  
+  especie: string
+  som():string
+}
+class Gato implements Animal{
+  tipoDePelagem: string
+  nome: string
+  especie: string
+  som(){ console.log(`O som feito por este animal é `)}
 }
