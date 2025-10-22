e27666215c3e0792d439527963aeaaa555fbaf29
Rafael Machado <rafaelmachadocd37@gmail.com>
2025-10-22 15:09:42 -0300

    Testes

--- ex04.ts
+++ ex04.ts
@@ -2,8 +2,43 @@
 O usuário deve ser capaz de informar o número do canal e aumentar ou diminuir o volume.
 Certifique-se de que o número do canal e o nível do volume permanecem dentro de faixas válidas.*/
 
-const televisor: { numCanal: number; incVolume: number; decVolume: number } = {
-  numCanal: 16, 
-  incVolume: 21,
-  decVolume: 22
-};
+class televisor {
+  numCanal: number
+  volumeAtual: number
+
+  constructor(numCanal: number, volumeAtual: number){
+  this.numCanal = numCanal
+  this.volumeAtual = volumeAtual
+  }
+  
+  mudarCanal(a: number){
+    if(a < 0 || a >= 50){
+      console.log("Digite um canal válido")
+    } else {
+      console.log(`O canal escolhido foi ${a}`)
+    }
+  }
+  
+  aumVolume(a: number){
+    if(a + this.volumeAtual > 100){
+      console.log("O volume máximo é 100, tente novamente com um valor menor")
+    } else if (a + this.volumeAtual < 0){
+      console.log("O volume mínimo é 0 (mudo), tente novamente com um valor maior")
+    } else {
+      console.log(`O volume atual é de ${a + this.volumeAtual}`)
+    }
+  }
+
+  dimVolume(a: number){
+    if(this.volumeAtual - a < 0){
+      console.log("O volume mínimo é 0 (mudo), tente novamente")
+    } else {
+      console.log(`O volume atual é de ${this.volumeAtual - a}`)
+    }
+  }
+}
+
+const televisor1 = new televisor(4, 15)
+televisor1.mudarCanal(40)
+televisor1.aumVolume(15)
+televisor1.dimVolume(20)
