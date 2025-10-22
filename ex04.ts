94183f14a50d35a1835191b6fb0af510191ad76f
Rafael Machado <rafaelmachadocd37@gmail.com>
2025-10-22 00:31:08 -0300

    minor changes

---
+++ ex04.ts
@@ -0,0 +1,9 @@
+/*Faça um programa que simule um televisor criando-o como um objeto:
+O usuário deve ser capaz de informar o número do canal e aumentar ou diminuir o volume.
+Certifique-se de que o número do canal e o nível do volume permanecem dentro de faixas válidas.*/
+
+const televisor: { numCanal: number; incVolume: number; decVolume: number } = {
+  numCanal: 16, 
+  incVolume: 21,
+  decVolume: 22
+};
