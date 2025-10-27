a61aab4c6dc9288f3a49d28309e54d497126deff
Rafael Machado <rafaelmachadocd37@gmail.com>
2025-10-27 20:27:34 -0300

    Minor changes

---
+++ ex05.ts
@@ -0,0 +1,43 @@
+/*Crie uma classe que modele um macaco:
+Atributos: Nome e bucho (estômago).
+Métodos: Comer, verBucho e digerir.
+Faça um programa e teste interativamente:
+Crie 2 macacos
+Alimente-os com 3 alimentos diferentes e verificando o conteúdo do estômago a cada refeição.
+Experimente fazer com que um macaco coma o outro. É possível criar um macaco canibal?*/
+
+class macaco {
+  nome: string
+  estomago[]: string
+
+  constructor(nome: string, estomago: string){
+    this.nome = nome
+    this.estomago = estomago
+
+  }
+
+  comer(a: string){
+    const 
+    
+
+
+  }
+
+
+  verEstomago(a: string){}
+
+
+  digerir(a: string){
+    this.estomago
+
+
+  }
+
+
+
+
+
+
+
+
+}
